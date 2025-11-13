e76f4b03c5730cfc911dad932acbf645608e24d1
Jefferson <surecjeff852@gmail.com>
2025-11-13 15:03:18 -0600

    configuracion del proyecto

---
+++ usuarios/usuarios/settings/local.py
@@ -0,0 +1,21 @@
+from .base import *
+    
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('USER'),
+        'PASSWORD':get_secret('PASSWORD'),
+        'HOST':'localhost',
+        'PORT':'5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
